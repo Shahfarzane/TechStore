1dee3b5b54fcafc83aa652fb4dfac22f0158f6da
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2020-11-10 09:24:36 +0100

    main page done

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var listOfProducts;
+var itemsInLocalStorage = localStorage.getItem("itemsInCart")
 
 /** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
@@ -18,28 +19,51 @@ function initSite() {
 }
 
 function addProductsToWebpage() {
-    console.log(listOfProducts);
- var container = document.createElement('div');
-var itemCard = document.createElement('div');
+
+    var main = document.getElementsByTagName("main")[0]
+    // main.className = "mains"
+    // main.style.height = "100%"
+    // main.style.marginTop = "15vh";
+    // main.style.textAlign = "center"
+
+
+
+    var container = document.createElement('div');
+    container.className = "container"
+    
 
     for (let i = 0; i < listOfProducts.length; i++) {
-        const item = listOfProducts[i];
+        var item = listOfProducts[i];
 
+
+
+var itemCard = document.createElement('div');
 var purchaseButton = document.createElement('button');
 var itemImage = document.createElement('img');
-var itemName = document.createElement('h1');
-var itemDescription = document.createElement('h5');
-var itemPrice = document.createElement('h5');
+var itemName = document.createElement('h2');
+var itemDescription = document.createElement('h6');
+var itemPrice = document.createElement('h6');
+var itemInfo = document.createElement('div')
+var buttonSpanText = document.createElement('span')
+var ButtonSpanIcon = document.createElement('span')
 
 
 
-purchaseButton.className= "btn btn-large btn-primary purchaseButton fas fa-cart-plus"
-purchaseButton.innerText = " Lägg till i kundvagnen"
-purchaseButton.onclick = function() {
-    console.log("purchase button pressed")
-  };
-container.className = 'container text-center';
-itemCard.className = "container-md"
+purchaseButton.className= "btn btn-large btn-primary purchaseButton "
+purchaseButton.appendChild(ButtonSpanIcon)
+
+purchaseButton.appendChild(buttonSpanText)
+ButtonSpanIcon.innerHTML = '<i class="fas fa-cart-plus"></i>'
+
+buttonSpanText.innerText = " Lägg till i kundvagnen"
+
+
+purchaseButton.data = item
+purchaseButton.addEventListener("click", function() {
+    addItemToLocalStorage(this.data)
+})
+    
+itemCard.className = "item-card"
 itemImage.style.height = "50vh"
 itemImage.style.width = "auto"
 
@@ -49,26 +73,32 @@ itemDescription.innerText = item.description
 itemPrice.innerText = item.price +" "+"kr";
 
 
+itemInfo.appendChild(itemName)
+itemInfo.appendChild(itemDescription)
+itemInfo.appendChild(itemImage)
+itemInfo.appendChild(itemPrice)
+itemInfo.appendChild(purchaseButton)
 
-
-itemCard.appendChild(itemName)
-itemCard.appendChild(itemDescription)
-itemCard.appendChild(itemImage)
-itemCard.appendChild(itemPrice)
-itemCard.appendChild(purchaseButton)
-
-
-
-
-        
-    }
-    document.body.appendChild(container);
-
-    container.appendChild(itemCard)
-
-
-
+itemCard.appendChild(itemInfo)
+container.appendChild(itemCard)
+ main.appendChild(container)
 
 }
 
 
+}
+
+function addItemToLocalStorage(item){
+
+    if (!itemsInLocalStorage) {
+
+        localStorage.setItem("itemsInCart", "[]");
+    }
+
+    var addedToCart = JSON.parse(itemsInLocalStorage)
+    var phone = item;
+    addedToCart.push(phone)
+    localStorage.setItem("itemsInCart", JSON.stringify(addedToCart));
+    document.getElementById("counter").innerText = addedToCart.length
+
+}
